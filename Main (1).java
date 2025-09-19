96c98ebed4aeb8dc2cbb67190ce3890a161eb75f
isametrical <idecastromendes08@gmail.com>
2025-09-19 12:34:52 -0400

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,12 @@
+/*Isadora De Castro Mendes - September 2025 - Unit 0 Project - Desription: This unit project is to print out initials. My code spells out my initials "IDM" using multiple lines. I carefully spaced each letter out on each line so the letters align with one another. The final result of my code was the expected outcome.*/
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("IIIIIIIIIIIII    DDDDDDD        MMMM      MMMM");
+    System.out.println("     II          DD     DD      MM MM    MM MM");
+    System.out.println("     II          DD      DD     MM  MM  MM  MM");
+    System.out.println("     II          DD       DD    MM    MM    MM");
+    System.out.println("     II          DD      DD     MM          MM");
+    System.out.println("     II          DD     DD      MM          MM");
+    System.out.println("     II          DD    DD       MM          MM");
+    System.out.println("IIIIIIIIIIIII    DD DD          MM          MM");}}
